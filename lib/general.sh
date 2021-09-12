eb7b5ad7eb537f35e246b81c5385642fb911891a
yunsur <yunsur.shi@gmail.com>
2021-09-12 13:57:21 +0800

    v20210912

--- lib/general.sh
+++ lib/general.sh
@@ -1150,13 +1150,11 @@ prepare_host()
 	if [[ ! -f /etc/apt/sources.list.d/aptly.list ]]; then
 		display_alert "Updating from external repository" "aptly" "info"
 		if [ x"" != x"${http_proxy}" ]; then
-			apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --keyserver-options http-proxy="${http_proxy}" --recv-keys ED75B5A4483DA07C >/dev/null 2>&1
-			apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --keyserver-options http-proxy="${http_proxy}" --recv-keys ED75B5A4483DA07C >/dev/null 2>&1
+			curl -fsSL -x $http_proxy https://www.aptly.info/pubkey.txt | gpg --dearmor | tee /usr/share/keyrings/aptly-archive-keyring.gpg >/dev/null 2>&1
 		else
-			apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys ED75B5A4483DA07C >/dev/null 2>&1
-			apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys ED75B5A4483DA07C >/dev/null 2>&1
+			curl -fsSL https://www.aptly.info/pubkey.txt | gpg --dearmor | tee /usr/share/keyrings/aptly-archive-keyring.gpg >/dev/null 2>&1
 		fi
-		echo "deb http://repo.aptly.info/ nightly main" > /etc/apt/sources.list.d/aptly.list
+		echo "deb [signed-by=/usr/share/keyrings/aptly-archive-keyring.gpg] http://repo.aptly.info/ nightly main" > /etc/apt/sources.list.d/aptly.list
 	else
 		sed "s/squeeze/nightly/" -i /etc/apt/sources.list.d/aptly.list
 	fi
