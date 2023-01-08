fb8e3c83f139e6b363a2454a2e017f2e981041c1
ruan.lj <ruan.lj@foxmail.com>
2023-01-08 11:32:21 +0800

    Fix: extensions/bluez-firmware.sh repo path

--- extensions/bluez-firmware.sh
+++ extensions/bluez-firmware.sh
@@ -3,7 +3,7 @@ function fetch_sources_tools__bluez_firmware() {
 }
 
 function post_family_tweaks__install_bluez_firmware() {
-    cp "${SRC}"/cache/sources/bluez-firmware-git/broadcom/BCM4345C0.hcd "${SDCARD}"/lib/firmware/brcm/BCM4345C0.hcd
+    cp "${SRC}"/cache/sources/bluez-firmware/broadcom/BCM4345C0.hcd "${SDCARD}"/lib/firmware/brcm/BCM4345C0.hcd
     cp "${SDCARD}"/lib/firmware/brcm/brcmfmac43455-sdio.txt "${SDCARD}"/lib/firmware/brcm/brcmfmac43455-sdio.phicomm,n1.txt
     cp "${SDCARD}"/lib/firmware/brcm/brcmfmac43455-sdio.bin "${SDCARD}"/lib/firmware/brcm/brcmfmac43455-sdio.phicomm,n1.bin
 }
